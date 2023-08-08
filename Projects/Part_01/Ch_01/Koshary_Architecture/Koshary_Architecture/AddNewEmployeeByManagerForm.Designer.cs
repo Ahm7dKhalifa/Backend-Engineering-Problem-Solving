1097af83c2f1339a2d8352cbfc9e3b167a931433
Ahmed Khalifa <a.akhalifa@dev.thiqah.sa>
2023-08-08 21:19:47 +0300

    Projects\Part_01\Ch_01\Koshary_Architecture

---
+++ Projects/Part_01/Ch_01/Koshary_Architecture/Koshary_Architecture/AddNewEmployeeByManagerForm.Designer.cs
@@ -0,0 +1,357 @@
+﻿namespace Koshary_Architecture
+{
+    partial class AddNewEmployeeByManagerForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            EmployeeNameTextBox = new TextBox();
+            EmployeeBirthdatePicker = new DateTimePicker();
+            CountryTextBox = new TextBox();
+            CityTextBox = new TextBox();
+            EmployeeStreetAndBuildingNumberTextBox = new TextBox();
+            EmployeeEmailTextBox = new TextBox();
+            EmployeePhoneNumberTextBox = new TextBox();
+            EmployeeSkillTextBox_1 = new TextBox();
+            EmployeeSkillTextBox_2 = new TextBox();
+            EmployeeSkillTextBox_3 = new TextBox();
+            EmployeeSkillTextBox_4 = new TextBox();
+            EmployeeSkillTextBox_5 = new TextBox();
+            EmployeeSkillTextBox_6 = new TextBox();
+            EmployeeNameLabel = new Label();
+            EmployeeBirthdateLabel = new Label();
+            EmployeeEmailLabel = new Label();
+            EmployeePhoneNumberLabel = new Label();
+            EmployeeAddressLabel = new Label();
+            CountryLabel = new Label();
+            CityLabel = new Label();
+            StreetAndBuildingNumberLabel = new Label();
+            EmployeeSkillsLabel = new Label();
+            EmployeeSkill_1_Label = new Label();
+            EmployeeSkill_2_Label = new Label();
+            EmployeeSkill_3_Label = new Label();
+            EmployeeSkill_4_Label = new Label();
+            EmployeeSkill_5_Label = new Label();
+            EmployeeSkill_6_Label = new Label();
+            SuspendLayout();
+            // 
+            // EmployeeNameTextBox
+            // 
+            EmployeeNameTextBox.Location = new Point(129, 92);
+            EmployeeNameTextBox.Name = "EmployeeNameTextBox";
+            EmployeeNameTextBox.Size = new Size(482, 31);
+            EmployeeNameTextBox.TabIndex = 0;
+            // 
+            // EmployeeBirthdatePicker
+            // 
+            EmployeeBirthdatePicker.Location = new Point(709, 92);
+            EmployeeBirthdatePicker.Name = "EmployeeBirthdatePicker";
+            EmployeeBirthdatePicker.Size = new Size(454, 31);
+            EmployeeBirthdatePicker.TabIndex = 1;
+            // 
+            // CountryTextBox
+            // 
+            CountryTextBox.Location = new Point(317, 280);
+            CountryTextBox.Name = "CountryTextBox";
+            CountryTextBox.Size = new Size(294, 31);
+            CountryTextBox.TabIndex = 2;
+            // 
+            // CityTextBox
+            // 
+            CityTextBox.Location = new Point(648, 280);
+            CityTextBox.Name = "CityTextBox";
+            CityTextBox.Size = new Size(266, 31);
+            CityTextBox.TabIndex = 3;
+            // 
+            // EmployeeStreetAndBuildingNumberTextBox
+            // 
+            EmployeeStreetAndBuildingNumberTextBox.Location = new Point(956, 283);
+            EmployeeStreetAndBuildingNumberTextBox.Name = "EmployeeStreetAndBuildingNumberTextBox";
+            EmployeeStreetAndBuildingNumberTextBox.Size = new Size(266, 31);
+            EmployeeStreetAndBuildingNumberTextBox.TabIndex = 4;
+            // 
+            // EmployeeEmailTextBox
+            // 
+            EmployeeEmailTextBox.Location = new Point(129, 174);
+            EmployeeEmailTextBox.Name = "EmployeeEmailTextBox";
+            EmployeeEmailTextBox.Size = new Size(482, 31);
+            EmployeeEmailTextBox.TabIndex = 5;
+            // 
+            // EmployeePhoneNumberTextBox
+            // 
+            EmployeePhoneNumberTextBox.Location = new Point(709, 174);
+            EmployeePhoneNumberTextBox.Name = "EmployeePhoneNumberTextBox";
+            EmployeePhoneNumberTextBox.Size = new Size(454, 31);
+            EmployeePhoneNumberTextBox.TabIndex = 6;
+            // 
+            // EmployeeSkillTextBox_1
+            // 
+            EmployeeSkillTextBox_1.Location = new Point(317, 393);
+            EmployeeSkillTextBox_1.Name = "EmployeeSkillTextBox_1";
+            EmployeeSkillTextBox_1.Size = new Size(294, 31);
+            EmployeeSkillTextBox_1.TabIndex = 7;
+            // 
+            // EmployeeSkillTextBox_2
+            // 
+            EmployeeSkillTextBox_2.Location = new Point(648, 393);
+            EmployeeSkillTextBox_2.Name = "EmployeeSkillTextBox_2";
+            EmployeeSkillTextBox_2.Size = new Size(266, 31);
+            EmployeeSkillTextBox_2.TabIndex = 8;
+            // 
+            // EmployeeSkillTextBox_3
+            // 
+            EmployeeSkillTextBox_3.Location = new Point(956, 393);
+            EmployeeSkillTextBox_3.Name = "EmployeeSkillTextBox_3";
+            EmployeeSkillTextBox_3.Size = new Size(266, 31);
+            EmployeeSkillTextBox_3.TabIndex = 9;
+            // 
+            // EmployeeSkillTextBox_4
+            // 
+            EmployeeSkillTextBox_4.Location = new Point(317, 476);
+            EmployeeSkillTextBox_4.Name = "EmployeeSkillTextBox_4";
+            EmployeeSkillTextBox_4.Size = new Size(294, 31);
+            EmployeeSkillTextBox_4.TabIndex = 10;
+            // 
+            // EmployeeSkillTextBox_5
+            // 
+            EmployeeSkillTextBox_5.Location = new Point(648, 476);
+            EmployeeSkillTextBox_5.Name = "EmployeeSkillTextBox_5";
+            EmployeeSkillTextBox_5.Size = new Size(266, 31);
+            EmployeeSkillTextBox_5.TabIndex = 11;
+            // 
+            // EmployeeSkillTextBox_6
+            // 
+            EmployeeSkillTextBox_6.Location = new Point(956, 476);
+            EmployeeSkillTextBox_6.Name = "EmployeeSkillTextBox_6";
+            EmployeeSkillTextBox_6.Size = new Size(266, 31);
+            EmployeeSkillTextBox_6.TabIndex = 12;
+            // 
+            // EmployeeNameLabel
+            // 
+            EmployeeNameLabel.AutoSize = true;
+            EmployeeNameLabel.Location = new Point(133, 57);
+            EmployeeNameLabel.Name = "EmployeeNameLabel";
+            EmployeeNameLabel.Size = new Size(147, 25);
+            EmployeeNameLabel.TabIndex = 13;
+            EmployeeNameLabel.Text = "Employee Name ";
+            // 
+            // EmployeeBirthdateLabel
+            // 
+            EmployeeBirthdateLabel.AutoSize = true;
+            EmployeeBirthdateLabel.Location = new Point(709, 57);
+            EmployeeBirthdateLabel.Name = "EmployeeBirthdateLabel";
+            EmployeeBirthdateLabel.Size = new Size(171, 25);
+            EmployeeBirthdateLabel.TabIndex = 14;
+            EmployeeBirthdateLabel.Text = "Employee Birthdate ";
+            // 
+            // EmployeeEmailLabel
+            // 
+            EmployeeEmailLabel.AutoSize = true;
+            EmployeeEmailLabel.Location = new Point(129, 146);
+            EmployeeEmailLabel.Name = "EmployeeEmailLabel";
+            EmployeeEmailLabel.Size = new Size(137, 25);
+            EmployeeEmailLabel.TabIndex = 15;
+            EmployeeEmailLabel.Text = "Employee Email";
+            // 
+            // EmployeePhoneNumberLabel
+            // 
+            EmployeePhoneNumberLabel.AutoSize = true;
+            EmployeePhoneNumberLabel.Location = new Point(709, 146);
+            EmployeePhoneNumberLabel.Name = "EmployeePhoneNumberLabel";
+            EmployeePhoneNumberLabel.Size = new Size(215, 25);
+            EmployeePhoneNumberLabel.TabIndex = 16;
+            EmployeePhoneNumberLabel.Text = "Employee Phone Number";
+            // 
+            // EmployeeAddressLabel
+            // 
+            EmployeeAddressLabel.AutoSize = true;
+            EmployeeAddressLabel.Location = new Point(133, 280);
+            EmployeeAddressLabel.Name = "EmployeeAddressLabel";
+            EmployeeAddressLabel.Size = new Size(169, 25);
+            EmployeeAddressLabel.TabIndex = 17;
+            EmployeeAddressLabel.Text = "Employee Address :";
+            // 
+            // CountryLabel
+            // 
+            CountryLabel.AutoSize = true;
+            CountryLabel.Location = new Point(345, 252);
+            CountryLabel.Name = "CountryLabel";
+            CountryLabel.Size = new Size(75, 25);
+            CountryLabel.TabIndex = 18;
+            CountryLabel.Text = "Country";
+            // 
+            // CityLabel
+            // 
+            CityLabel.AutoSize = true;
+            CityLabel.Location = new Point(648, 252);
+            CityLabel.Name = "CityLabel";
+            CityLabel.Size = new Size(42, 25);
+            CityLabel.TabIndex = 19;
+            CityLabel.Text = "City";
+            // 
+            // StreetAndBuildingNumberLabel
+            // 
+            StreetAndBuildingNumberLabel.AutoSize = true;
+            StreetAndBuildingNumberLabel.Location = new Point(956, 255);
+            StreetAndBuildingNumberLabel.Name = "StreetAndBuildingNumberLabel";
+            StreetAndBuildingNumberLabel.Size = new Size(231, 25);
+            StreetAndBuildingNumberLabel.TabIndex = 20;
+            StreetAndBuildingNumberLabel.Text = "Street and Building Number";
+            // 
+            // EmployeeSkillsLabel
+            // 
+            EmployeeSkillsLabel.AutoSize = true;
+            EmployeeSkillsLabel.Location = new Point(137, 399);
+            EmployeeSkillsLabel.Name = "EmployeeSkillsLabel";
+            EmployeeSkillsLabel.Size = new Size(143, 25);
+            EmployeeSkillsLabel.TabIndex = 21;
+            EmployeeSkillsLabel.Text = "Employee Skills :";
+            // 
+            // EmployeeSkill_1_Label
+            // 
+            EmployeeSkill_1_Label.AutoSize = true;
+            EmployeeSkill_1_Label.Location = new Point(317, 368);
+            EmployeeSkill_1_Label.Name = "EmployeeSkill_1_Label";
+            EmployeeSkill_1_Label.Size = new Size(58, 25);
+            EmployeeSkill_1_Label.TabIndex = 22;
+            EmployeeSkill_1_Label.Text = "Skill 1";
+            // 
+            // EmployeeSkill_2_Label
+            // 
+            EmployeeSkill_2_Label.AutoSize = true;
+            EmployeeSkill_2_Label.Location = new Point(648, 365);
+            EmployeeSkill_2_Label.Name = "EmployeeSkill_2_Label";
+            EmployeeSkill_2_Label.Size = new Size(58, 25);
+            EmployeeSkill_2_Label.TabIndex = 23;
+            EmployeeSkill_2_Label.Text = "Skill 2";
+            // 
+            // EmployeeSkill_3_Label
+            // 
+            EmployeeSkill_3_Label.AutoSize = true;
+            EmployeeSkill_3_Label.Location = new Point(956, 365);
+            EmployeeSkill_3_Label.Name = "EmployeeSkill_3_Label";
+            EmployeeSkill_3_Label.Size = new Size(58, 25);
+            EmployeeSkill_3_Label.TabIndex = 24;
+            EmployeeSkill_3_Label.Text = "Skill 3";
+            // 
+            // EmployeeSkill_4_Label
+            // 
+            EmployeeSkill_4_Label.AutoSize = true;
+            EmployeeSkill_4_Label.Location = new Point(317, 448);
+            EmployeeSkill_4_Label.Name = "EmployeeSkill_4_Label";
+            EmployeeSkill_4_Label.Size = new Size(58, 25);
+            EmployeeSkill_4_Label.TabIndex = 25;
+            EmployeeSkill_4_Label.Text = "Skill 4";
+            // 
+            // EmployeeSkill_5_Label
+            // 
+            EmployeeSkill_5_Label.AutoSize = true;
+            EmployeeSkill_5_Label.Location = new Point(648, 448);
+            EmployeeSkill_5_Label.Name = "EmployeeSkill_5_Label";
+            EmployeeSkill_5_Label.Size = new Size(58, 25);
+            EmployeeSkill_5_Label.TabIndex = 26;
+            EmployeeSkill_5_Label.Text = "Skill 5";
+            // 
+            // EmployeeSkill_6_Label
+            // 
+            EmployeeSkill_6_Label.AutoSize = true;
+            EmployeeSkill_6_Label.Location = new Point(956, 448);
+            EmployeeSkill_6_Label.Name = "EmployeeSkill_6_Label";
+            EmployeeSkill_6_Label.Size = new Size(58, 25);
+            EmployeeSkill_6_Label.TabIndex = 27;
+            EmployeeSkill_6_Label.Text = "Skill 6";
+            // 
+            // AddNewEmployeeByManagerForm
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1385, 673);
+            Controls.Add(EmployeeSkill_6_Label);
+            Controls.Add(EmployeeSkill_5_Label);
+            Controls.Add(EmployeeSkill_4_Label);
+            Controls.Add(EmployeeSkill_3_Label);
+            Controls.Add(EmployeeSkill_2_Label);
+            Controls.Add(EmployeeSkill_1_Label);
+            Controls.Add(EmployeeSkillsLabel);
+            Controls.Add(StreetAndBuildingNumberLabel);
+            Controls.Add(CityLabel);
+            Controls.Add(CountryLabel);
+            Controls.Add(EmployeeAddressLabel);
+            Controls.Add(EmployeePhoneNumberLabel);
+            Controls.Add(EmployeeEmailLabel);
+            Controls.Add(EmployeeBirthdateLabel);
+            Controls.Add(EmployeeNameLabel);
+            Controls.Add(EmployeeSkillTextBox_6);
+            Controls.Add(EmployeeSkillTextBox_5);
+            Controls.Add(EmployeeSkillTextBox_4);
+            Controls.Add(EmployeeSkillTextBox_3);
+            Controls.Add(EmployeeSkillTextBox_2);
+            Controls.Add(EmployeeSkillTextBox_1);
+            Controls.Add(EmployeePhoneNumberTextBox);
+            Controls.Add(EmployeeEmailTextBox);
+            Controls.Add(EmployeeStreetAndBuildingNumberTextBox);
+            Controls.Add(CityTextBox);
+            Controls.Add(CountryTextBox);
+            Controls.Add(EmployeeBirthdatePicker);
+            Controls.Add(EmployeeNameTextBox);
+            Name = "AddNewEmployeeByManagerForm";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox EmployeeNameTextBox;
+        private DateTimePicker EmployeeBirthdatePicker;
+        private TextBox CountryTextBox;
+        private TextBox CityTextBox;
+        private TextBox EmployeeStreetAndBuildingNumberTextBox;
+        private TextBox EmployeeEmailTextBox;
+        private TextBox EmployeePhoneNumberTextBox;
+        private TextBox EmployeeSkillTextBox_1;
+        private TextBox EmployeeSkillTextBox_2;
+        private TextBox EmployeeSkillTextBox_3;
+        private TextBox EmployeeSkillTextBox_4;
+        private TextBox EmployeeSkillTextBox_5;
+        private TextBox EmployeeSkillTextBox_6;
+        private Label EmployeeNameLabel;
+        private Label EmployeeBirthdateLabel;
+        private Label EmployeeEmailLabel;
+        private Label EmployeePhoneNumberLabel;
+        private Label EmployeeAddressLabel;
+        private Label CountryLabel;
+        private Label CityLabel;
+        private Label StreetAndBuildingNumberLabel;
+        private Label EmployeeSkillsLabel;
+        private Label EmployeeSkill_1_Label;
+        private Label EmployeeSkill_2_Label;
+        private Label EmployeeSkill_3_Label;
+        private Label EmployeeSkill_4_Label;
+        private Label EmployeeSkill_5_Label;
+        private Label EmployeeSkill_6_Label;
+    }
+}
---
+++ Projects/Part_01/Ch_01/Koshary_Architecture/Koshary_Architecture/AddNewEmployeeByManagerForm.cs
@@ -0,0 +1,34 @@
+namespace Koshary_Architecture
+{
+    public partial class AddNewEmployeeByManagerForm : Form
+    {
+        public AddNewEmployeeByManagerForm()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void CheckEmployeeAgeIsEqualOrGreaterThan21Years(DateTime birthdate, List<string> errors)
+        {
+
+        }
+
+        private void CheckEmailHasCorrectFormat(string email, List<string> errors)
+        {
+
+        }
+
+        private void CheckEmployeeHasAtLeastOneSkill(string firstSkill,
+                                                     string secondSkill,
+                                                     string thirdSkill,
+                                                     List<string> errors)
+        {
+
+        }
+
+
+
+
+    }
+}
